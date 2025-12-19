b1e2fe540745f0e288f56562a09469e327d2f4e4
jayvon33 <stephenyayravondee@gmail.com>
2025-12-19 17:56:33 +0000

    Update MENU_IMAGE_URL and ALIVE_IMG URLs

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/j2h8dg.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/o2zvp8.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
     
@@ -54,7 +54,7 @@ OWNER_NAME: process.env.OWNER_NAME || "Jay von",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© POWERED LIMKID-X KING*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/adymbp.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/o2zvp8.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> Powered by *LIMKID-X TECH*⚡",
 // add alive msg here 
